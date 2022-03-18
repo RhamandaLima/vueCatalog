57b6182489554c8415021b9f57520844c6a37899
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-03-18 10:05:05 -0300

    Add ajustes do rodapé

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -1,50 +1,24 @@
 <template>
-  <v-footer
-    dark
-    padless
-  >
-    <v-card
-      flat
-      tile
-      class="blue-grey darken-4 white--text text-center"
+  <v-footer padless>
+    <v-col
+      color="blue-grey darken-4"
+      class="rodape text-center"
+      cols="12"
     >
-      <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon  
-          href="https://github.com/RhamandaLima/vueCatalog"
-        > 
-        <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
-
-      <v-card-text class="white--text pt-0">
-        Projeto elaborado para o desafio do 10ª Hackathon promovido pela Iteris.
-      </v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Equipe 10</strong>
-      </v-card-text>
-    </v-card>
+      {{ new Date().getFullYear() }} — <strong>Time 10</strong>
+    </v-col>
   </v-footer>
 </template>
 
 <script>
   export default {
-    data: () => ({
-      icons: [
-        'mdi-github',
-      ],
-    }),
   }
 </script>
 
 <style scoped>
-
+ .rodape {
+   background-color:#263238;
+   color: #FAFAFA; 
+   font-size: 14px;
+ }
 </style>
