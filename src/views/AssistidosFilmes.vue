cd691f205be11f6b6f5b4baae75aa4bd574647c9
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-03-18 23:40:33 -0300

    Add alt teste

--- src/views/AssistidosFilmes.vue
+++ src/views/AssistidosFilmes.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="catalogo-filmes">
-        <h2 class="text-center ma-4">FILMES ASSISTIDOS</h2>
+        <h2 class="text-center ma-4">FILMES VISTOS</h2>
         <h3 class="text-center ma-4">Quantidade de filmes assistidos: {{contador}}</h3>
         <v-card
             max-width="300px"
